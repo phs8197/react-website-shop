705e3cf3e3e8752aa9da0b6c1a99454dcace8fe6
Molly <107332652+adjlfkld@users.noreply.github.com>
2022-12-23 23:26:20 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
+import data from "./data.js";
+import { useState } from "react";
 
 function App() {
+  let [product] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light" className="Navbar">
+        <Container>
+          <Navbar.Brand href="#home" className="logo">
+            Hay
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <Nav.Link href="#Shop">Shop</Nav.Link>
+            <Nav.Link href="#Shop">Collection</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          {product.map(function (a, i) {
+            return <Product product={a} key={i}></Product>;
+          })}
+        </Row>
+      </Container>
     </div>
+  );
+}
+function Product(props) {
+  return (
+    <>
+      <Col sm>
+        <img src={props.product.img} width="200px" />
+        <h4>{props.product.title}</h4>
+        <p>{props.product.price}</p>
+      </Col>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,26 @@
+let data = [
+  {
+    id: 0,
+    img: "/img/12.jpg",
+    title: "Cover Armchair",
+    content: "Born in France",
+    price: "$320.00",
+  },
+
+  {
+    id: 1,
+    img: "/img/13.jpg",
+    title: "Riley Coffee Tables",
+    content: "Born in Seoul",
+    price: "$459.43",
+  },
+
+  {
+    id: 2,
+    img: "/img/14.jpg",
+    title: "Salt Table Lamp",
+    content: "Born in the States",
+    price: "$189.43",
+  },
+];
+export default data;
