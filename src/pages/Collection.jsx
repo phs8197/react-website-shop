cefccc52d9641558320148ff0a8ba67b7b98f275
Molly <107332652+adjlfkld@users.noreply.github.com>
2022-12-30 23:55:48 +0900

    add collection

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
-function Collection() {
-  return <div>Collection</div>;
+import { Container, Col, Row } from "react-bootstrap";
+function Collection(props) {
+  return (
+    <>
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          {props.product.map(function (a) {
+            return <Product product={a}></Product>;
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+}
+function Product(props) {
+  return (
+    <>
+      <Col sm>
+        <img src={props.product.img} width="200px" />
+        <h4>{props.product.title}</h4>
+        <p>{props.product.price}</p>
+      </Col>
+    </>
+  );
 }
 
 export default Collection;
